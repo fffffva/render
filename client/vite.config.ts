62d2dbfb7b126c73068a255acbee5f435edd8cc2
ofer <ofergavri@gmail.com>
2023-07-16 22:50:03 +0300

    fix

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,26 +1,24 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const API_URL = process.env.NODE_ENV === 'production' ? 'https://rolling-2szg.onrender.com' : 'http://localhost:5000'
-console.log('API_URL', API_URL[process.env.MODE || 'development'], process.env.MODE);
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: API_URL,
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+export default defineConfig(({ mode }) => {
+  const API_URL = mode === 'production' ? 'https://rolling-2szg.onrender.com' : 'http://localhost:5000';
+
+  return {
+    plugins: [react()],
+    server: {
+      port: 3000,
+      proxy: {
+        '/api': {
+          target: API_URL,
+          changeOrigin: true,
+        },
       },
     },
-  },
-  build: {
-    target: 'esnext',
-    outDir: './dist',
-    sourcemap: true,
-  },
-  define: {
-    'process.env': {}, // Prevent process.env from being replaced during the build
-  },
+    build: {
+      target: 'esnext',
+      outDir: './dist',
+      sourcemap: true,
+    },
+  };
 });

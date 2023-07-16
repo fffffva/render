236d21bf8e5eb6d0dc569a07667ab8b99c5ca356
ofer <ofergavri@gmail.com>
2023-07-16 22:51:14 +0300

    fix

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
+  console.log('mode', mode);
   const API_URL = mode === 'production' ? 'https://rolling-2szg.onrender.com' : 'http://localhost:5000';
 
   return {

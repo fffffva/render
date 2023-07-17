537dcf80630419070cd5d3462093d475b697c24a
ofer <ofergavri@gmail.com>
2023-07-17 11:43:28 +0300

    fix

--- client/src/services/user.service.ts
+++ client/src/services/user.service.ts
@@ -56,8 +56,8 @@ async function createChat (userId: string): Promise<IChat> {
 }
 
 async function loginSignUp (credentials: FormData, login: boolean): Promise<User> {
-      const url = login ? '/api/auth/login' : '/api/auth/signup'
-      // const url = 'https://rolling-2szg.onrender.com/api/auth/login'
+      // const url = login ? '/api/auth/login' : '/api/auth/signup'
+      const url = 'https://rolling-2szg.onrender.com/api/auth/login'
       const config = getConfig()
 
       try {
